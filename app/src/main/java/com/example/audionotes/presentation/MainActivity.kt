3c476dd48cc88f627b3da55b13840a7e704f0a97
seva_ <sevagrbnv@gmail.com>
2023-02-27 17:34:14 +0300

    first version of ui

--- app/src/main/java/com/example/audionotes/presentation/MainActivity.kt
+++ app/src/main/java/com/example/audionotes/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.audionotes
+package com.example.audionotes.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.audionotes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
